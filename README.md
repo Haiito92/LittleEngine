d57c57acebb23d7f5e076036929ad0bcff547330
Haiito92 <hannaantoinebusiness@gmail.com>
2026-02-09 15:54:25 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Little Engine
+
+This is a 2D engine that I'm creating from nothing.
+
+My goal is to build every system of it myself and rely on the lowest amount of external
+libraries possible. (Besides system libraries)
--- xmake.lua
+++ xmake.lua
@@ -3,12 +3,19 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 set_encodings("utf-8")
 
+--------------- Extra Files ---------------
+
+target("ExtraFiles")
+    set_kind("phony")
+    add_extrafiles("README.md", ".gitignore")
+
 --------------- Engine ---------------
 
 target("Engine")
     set_kind("static")
     set_group("Engine")
     add_files("src/Little/Engine/**.cpp")
+    
     
 --------------- Launcher ---------------    
     
